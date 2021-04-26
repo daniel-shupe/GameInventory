44ffec8dc520dc412d699798e826e9accb2e23ff
Daniel Shupe <dlshupe@gmail.com>
2021-04-25 22:22:08 -0400

    Add project files.

---
+++ Models/DisplayGameModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameInventory.Models
+{
+    public class DisplayGameModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public bool Located { get; set; }
+        [Required]
+        public string Platform { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        public string Genre { get; set; }
+    }
+}
